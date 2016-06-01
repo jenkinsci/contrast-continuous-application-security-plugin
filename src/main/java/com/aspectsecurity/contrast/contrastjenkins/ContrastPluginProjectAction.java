d3277ddd70f8afee03d02404fb83b068fff3ff7f
Justin Leo <justin.leo@contrastsecurity.com>
2016-06-01 12:47:53 -0400

    Added two stacked bar charts, added project action to show charts, added build actions to save trace data

---
+++ src/main/java/com/aspectsecurity/contrast/contrastjenkins/ContrastPluginProjectAction.java
@@ -0,0 +1,69 @@
+package com.aspectsecurity.contrast.contrastjenkins;
+
+import com.aspectsecurity.contrast.contrastjenkins.plots.SeverityFrequencyPlot;
+import com.aspectsecurity.contrast.contrastjenkins.plots.VulnerabilityFrequencyPlot;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.Action;
+import hudson.util.Graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContrastPluginProjectAction implements Action {
+
+    private AbstractProject<?, ?> project;
+
+    public ContrastPluginProjectAction(final AbstractProject<?, ?> project) {
+        this.project = project;
+    }
+
+    @Override
+    public String getIconFileName() {
+        return "/plugin/testExample/img/project_icon.png";
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Contrast Project Project Action";
+    }
+
+    @Override
+    public String getUrlName() {
+        return "contrastProjectAction";
+    }
+
+    public AbstractProject<?, ?> getProject() {
+        return this.project;
+    }
+
+
+    public Graph getVulnerabilityGraph() {
+        return new VulnerabilityFrequencyPlot(project);
+    }
+
+    public Graph getSeverityGraph() {
+        return new SeverityFrequencyPlot(project);
+    }
+
+    public String getProjectName() {
+        return this.project.getName();
+    }
+
+    public List<String> getBuildResult() {
+        List<String> projectMessages = new ArrayList<String>();
+        List<? extends AbstractBuild<?, ?>> builds = project.getBuilds();
+        String projectMessage;
+
+        final Class<VulnerabilityFrequencyAction> buildClass = VulnerabilityFrequencyAction.class;
+
+        for (AbstractBuild<?, ?> currentBuild : builds) {
+            projectMessage = "Build #" + currentBuild.getAction(buildClass).getBuildNumber()
+                    + ": " + currentBuild.getAction(buildClass).toString(); // TODO print results
+
+            projectMessages.add(projectMessage);
+        }
+
+        return projectMessages;
+    }
+}
---
+++ src/main/java/com/aspectsecurity/contrast/contrastjenkins/plots/SeverityFrequencyPlot.java
@@ -0,0 +1,109 @@
+package com.aspectsecurity.contrast.contrastjenkins.plots;
+
+import com.aspectsecurity.contrast.contrastjenkins.VulnerabilityFrequencyAction;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.Run;
+import hudson.util.Graph;
+import hudson.util.RunList;
+import hudson.util.ShiftedCategoryAxis;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.RectangleInsets;
+
+import java.awt.*;
+import java.util.*;
+
+
+public class SeverityFrequencyPlot extends Graph {
+
+    private AbstractBuild<?, ?> build;
+    private AbstractProject<?, ?> project;
+
+    public SeverityFrequencyPlot(AbstractBuild<?, ?> build) {
+        super(Calendar.getInstance(), 640, 480);
+        this.build = build;
+    }
+
+    public SeverityFrequencyPlot(AbstractProject<?, ?> project) {
+        super(Calendar.getInstance(), 640, 480);
+        this.project = project;
+    }
+
+    @Override
+    protected JFreeChart createGraph() {
+        DefaultCategoryDataset dataset = createSeverityFrequencyDataset();
+
+        JFreeChart chart = ChartFactory.createStackedBarChart(
+                "Severity Frequency", // title
+                "Build Number", // x axis
+                "Severity Count", // y axis
+                dataset, // data
+                PlotOrientation.VERTICAL, // orientation
+                true, // legend
+                true,  // tooltips
+                false); //urls
+
+        chart.setBackgroundPaint(Color.white);
+
+        CategoryPlot plot = (CategoryPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.WHITE);
+        plot.setOutlinePaint(null);
+        plot.setRangeGridlinesVisible(true);
+        plot.setRangeGridlinePaint(Color.black);
+
+        CategoryAxis domainAxis = new ShiftedCategoryAxis(null);
+        plot.setDomainAxis(domainAxis);
+        domainAxis.setLowerMargin(0.0);
+        domainAxis.setUpperMargin(0.0);
+        domainAxis.setCategoryMargin(0.0);
+
+        NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        rangeAxis.setAutoRange(true);
+
+        plot.setInsets(new RectangleInsets(0, 0, 0, 5.0));
+        return chart;
+    }
+
+    private DefaultCategoryDataset createSeverityFrequencyDataset() {
+        java.util.List<VulnerabilityFrequencyAction> actions = new ArrayList<>();
+        RunList<?> builds;
+
+        if (build != null) {
+            builds = build.getParent().getBuilds();
+        } else {
+            builds = project.getBuilds();
+        }
+
+        // Get all build actions
+        for (Run<?, ?> run : builds) {
+            VulnerabilityFrequencyAction action = run.getAction(VulnerabilityFrequencyAction.class);
+
+            if (action == null) {
+                continue;
+            }
+            actions.add(action);
+        }
+
+        // put them in chronological order
+        Collections.reverse(actions);
+
+        // build data setup
+        DefaultCategoryDataset ds = new DefaultCategoryDataset();
+        for (VulnerabilityFrequencyAction action : actions) {
+            Map<String, Integer> result = action.getResult().getSeverityResult();
+
+            for (String severity: result.keySet()) {
+                ds.addValue(result.get(severity), severity, Integer.toString(action.getBuildNumber()));
+            }
+        }
+        return ds;
+    }
+}
