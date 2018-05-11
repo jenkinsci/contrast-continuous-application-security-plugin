e9b88e7861a4b710330d4d4afe84ca6e50f64429
serge17 <sergii.borsuk@contrastsecurity.com>
2018-05-11 14:37:47 -0500

    CUST-1747

---
+++ src/main/java/com/aspectsecurity/contrast/contrastjenkins/Constants.java
@@ -0,0 +1,14 @@
+package com.aspectsecurity.contrast.contrastjenkins;
+
+public final class Constants {
+
+    public static final String VULNERABILITY_STATUS_AUTO_REMEDIATED = "Auto-Remediated";
+    public static final String VULNERABILITY_STATUS_CONFIRMED = "Confirmed";
+    public static final String VULNERABILITY_STATUS_SUSPICIOUS = "Suspicious";
+    public static final String VULNERABILITY_STATUS_REMEDIATED = "Remediated";
+    public static final String VULNERABILITY_STATUS_REPORTED = "Reported";
+    public static final String VULNERABILITY_STATUS_FIXED = "Fixed";
+    public static final String VULNERABILITY_STATUS_BEING_TRACKED = "Being+Tracked";
+    public static final String VULNERABILITY_STATUS_UNTRACKED = "Untracked";
+    public static final String VULNERABILITY_STATUS_NOT_A_PROBLEM = "NotAProblem";
+}
