2d021ef4b06f7a8e4f1ee8d9b2265ca1b3dbff5e
Bryan Ateo-an <bryan.ateoan@contrastsecurity.com>
2021-07-16 11:18:00 -0400

    disabled remote management when starting

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 clear
-mvn hpi:run -Djetty.port=8090 -Pjenkins,runsh -Djava.net.preferIPv4Stack=true
+mvn hpi:run -Djetty.port=8090 -Pjenkins,runsh -Djava.net.preferIPv4Stack=true -Djenkins.CLI.disabled=true
