add84b396026758d157a8acf35d5b19cea0c5f6d
arturoespinoza <sergioarturoespinoza@gmail.com>
2026-06-04 16:54:24 -0600

    SCAN-6460: Remove hardcoded JAVA_HOME from run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 clear
-JAVA_HOME=/Users/arturoespinoza/.sdkman/candidates/java/21.0.5-amzn mvn hpi:run -Djetty.port=8090 -Prunsh -Djava.net.preferIPv4Stack=true -Djenkins.CLI.disabled=true -Denforcer.skip=true
+mvn hpi:run -Djetty.port=8090 -Prunsh -Djava.net.preferIPv4Stack=true -Djenkins.CLI.disabled=true -Denforcer.skip=true
